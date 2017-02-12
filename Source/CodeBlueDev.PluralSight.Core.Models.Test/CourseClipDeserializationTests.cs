da3f7cff89c511ad42f6b97fc46617916847261f
CodeBlue <codebluedev@gmail.com>
2017-02-12 14:45:09 -0700

    Update PluralSight CourseClip Model to reflect changes made to JSON response.
    PluralSight updated the CourseClip JSON web response which breaks
    deserialization of the CourseClip model. Separated and updated tests in
    case there are other changes in the future specific to the CourseClip
    model.

---
+++ Source/CodeBlueDev.PluralSight.Core.Models.Test/CourseClipDeserializationTests.cs
@@ -0,0 +1,77 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CourseClipDeserializationTests.cs" company="CodeBlueDev">
+//   All rights reserved.
+// </copyright>
+// <summary>
+//   Tests deserialization of PluralSight Clip objects.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CodeBlueDev.PluralSight.Core.Models.Test
+{
+    using Newtonsoft.Json;
+
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests deserialization of PluralSight Clip objects.
+    /// </summary>
+    [TestFixture]
+    public class CourseClipDeserializationTests
+    {
+        /// <summary>
+        /// Tests if a Clip JSON block can be deserialized to a Clip model.
+        /// </summary>
+        [Test]
+        public void ClipJsonShouldDeserializeIntoClipModel()
+        {
+            // Arrange
+            const string Json = @"{
+                ""id"":""windows-forms-best-practices|mark-heath|windows-forms-best-practices-m1|windows-forms-best-practices-m1-01"",
+                ""title"":""Module Introduction"",
+                ""duration"":""PT57.051S"",
+                ""playerUrl"":""/player?course=windows-forms-best-practices&author=mark-heath&name=windows-forms-best-practices-m1&clip=0&mode=live""
+            }";
+
+            // Act
+            CourseClip clip = JsonConvert.DeserializeObject<CourseClip>(Json);
+
+            // Assert
+            Assert.IsNotNull(clip);
+            Assert.AreEqual("windows-forms-best-practices|mark-heath|windows-forms-best-practices-m1|windows-forms-best-practices-m1-01", clip.Id);
+            Assert.AreEqual("Module Introduction", clip.Title);
+            Assert.AreEqual("PT57.051S", clip.Duration);
+            Assert.AreEqual("/player?course=windows-forms-best-practices&author=mark-heath&name=windows-forms-best-practices-m1&clip=0&mode=live", clip.PlayerUrl);
+        }
+
+        /// <summary>
+        /// Tests if a Clip JSON block with multiple Clips can be deserialized to multiple Clip models.
+        /// </summary>
+        [Test]
+        public void ClipJsonWithMultipleClipsShouldDeserializeIntoMultipleClipModels()
+        {
+            // Arrange
+            const string Json = @"[
+                {
+                   ""id"":""windows-forms-best-practices|mark-heath|windows-forms-best-practices-m1|windows-forms-best-practices-m1-01"",
+                   ""title"":""Module Introduction"",
+                   ""duration"":""PT57.051S"",
+                   ""playerUrl"":""/player?course=windows-forms-best-practices&author=mark-heath&name=windows-forms-best-practices-m1&clip=0&mode=live""
+                },
+                {
+                   ""id"":""windows-forms-best-practices|mark-heath|windows-forms-best-practices-m1|windows-forms-best-practices-m1-02"",
+                   ""title"":""Is Windows Forms Dead?"",
+                   ""duration"":""PT1M36.572S"",
+                   ""playerUrl"":""/player?course=windows-forms-best-practices&author=mark-heath&name=windows-forms-best-practices-m1&clip=1&mode=live""
+                }
+            ]";
+
+            // Act
+            CourseClip[] clips = JsonConvert.DeserializeObject<CourseClip[]>(Json);
+
+            // Assert
+            Assert.IsNotNull(clips);
+            Assert.AreEqual(2, clips.Length);
+        }
+    }
+}
---
+++ Source/CodeBlueDev.PluralSight.Core.Models/CourseClip.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CourseClip.cs" company="CodeBlueDev">
+//   Copyright (c) CodeBlueDev. All rights reserved.
+// </copyright>
+// <summary>
+//   Represents a PluralSight Course's Module's clip.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CodeBlueDev.PluralSight.Core.Models
+{
+    using System;
+
+    /// <summary>
+    /// Represents a PluralSight Course's Module's clip.
+    /// </summary>
+    [Serializable]
+    public sealed class CourseClip
+    {
+        /// <summary>
+        /// Gets or sets the id of the PluralSight Course Clip.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title of the PluralSight Course Clip.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the duration of the PluralSight Course Clip.
+        /// </summary>
+        public string Duration { get; set; }
+
+        /// <summary>
+        /// Gets or sets the PluralSight player url of the PluralSight Course Clip.
+        /// </summary>
+        public string PlayerUrl { get; set; }
+    }
+}
