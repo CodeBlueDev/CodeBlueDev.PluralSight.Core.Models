12f7fe1e53a8948075b45b982b9f39b4d2ee0596
CodeBlue <codebluedev@gmail.com>
2017-02-13 19:40:08 -0700

    Update PluralSight CourseRating Model to reflect changes made to JSON response.
    PluralSight updated the CourseRating JSON web response. Separated and
    updated tests in case there are other changes in the future specific to
    the CourseRating model.

--- Source/CodeBlueDev.PluralSight.Core.Models/CourseRating.cs
+++ Source/CodeBlueDev.PluralSight.Core.Models/CourseRating.cs
@@ -18,38 +18,13 @@ namespace CodeBlueDev.PluralSight.Core.Models
     public sealed class CourseRating
     {
         /// <summary>
-        /// Gets or sets the current users rating for the course.
-        /// </summary>
-        public int CurrentUsersRating { get; set; }
-
-        /// <summary>
         /// Gets or sets the average rating for the course.
         /// </summary>
-        public decimal AverageRating { get; set; }
-
-        /// <summary>
-        /// Gets or sets the rating for the course.
-        /// </summary>
-        public decimal Rating { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether the user can rate this course.
-        /// </summary>
-        public bool CanRateThisCourse { get; set; }
-
-        /// <summary>
-        /// Gets or sets the course name.
-        /// </summary>
-        public string CourseName { get; set; }
+        public decimal Average { get; set; }
 
         /// <summary>
         /// Gets or sets the number of users that have rated the course.
         /// </summary>
-        public int NumberOfRaters { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether the user has rated the course.
-        /// </summary>
-        public bool HasUserRatedCourse { get; set; }
+        public int RatersCount { get; set; }
     }
 }
