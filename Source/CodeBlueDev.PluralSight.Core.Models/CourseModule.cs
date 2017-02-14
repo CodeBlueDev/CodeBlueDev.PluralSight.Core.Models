6e697dc54551f47a9e335372db75913699d8c4da
CodeBlue <codebluedev@gmail.com>
2017-02-13 19:42:25 -0700

    Create PluralSight CourseModule Model to reflect changes made to JSON response.
    PluralSight updated the Module JSON web response which breaks
    deserialization of the Module model. Separated and updated tests in
    case there are other changes in the future specific to the CourseModule
    model.

---
+++ Source/CodeBlueDev.PluralSight.Core.Models/CourseModule.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CourseModule.cs" company="CodeBlueDev">
+//   Copyright (c) CodeBlueDev. All rights reserved.
+// </copyright>
+// <summary>
+//   Represents a PluralSight Module.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CodeBlueDev.PluralSight.Core.Models
+{
+    using System;
+
+    /// <summary>
+    /// Represents a PluralSight Course's Module.
+    /// </summary>
+    [Serializable]
+    public sealed class CourseModule
+    {
+        /// <summary>
+        /// Gets or sets the id of the PluralSight Course Module.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title of the PluralSight Course Module.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the duration of the PluralSight Course Module.
+        /// </summary>
+        public string Duration { get; set; }
+
+        /// <summary>
+        /// Gets or sets the player url of the PluralSight Course Module.
+        /// </summary>
+        public string PlayerUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the clips of the PluralSight Course Module.
+        /// </summary>
+        public CourseClip[] Clips { get; set; }
+    }
+}
