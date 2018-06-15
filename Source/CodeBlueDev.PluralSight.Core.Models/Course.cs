562ae0cfc2de3ee334a38f378941ecc51c858e33
CodeBlueDev <9bedfd04@opayq.com>
2018-06-15 07:24:36 -0600

    Add course example to XmlDoc for reference

--- Source/CodeBlueDev.PluralSight.Core.Models/Course.cs
+++ Source/CodeBlueDev.PluralSight.Core.Models/Course.cs
@@ -15,6 +15,60 @@ namespace CodeBlueDev.PluralSight.Core.Models
     /// <summary>
     /// Represents a PluralSight Course.
     /// </summary>
+    /// <example>
+    /// "course": {
+    ///   "id": "windows-forms-best-practices",
+    ///   "courseId": "24f3e2ce-88bc-44f5-a9b7-3b0f326c3494",
+    ///   "slug": "windows-forms-best-practices",
+    ///   "publishedOn": "2014-09-27",
+    ///   "updatedOn": "2014-09-27",
+    ///   "displayDate": "2014-09-27",
+    ///   "title": "Windows Forms Best Practices",
+    ///   "shortDescription": "This course demonstrates several best practices for Windows Forms application development by gradually improving a demo application. Topics covered include resizing, layout, accessibility, maintainable code, localization, usability, testability, threading, exception handling, custom control creation, and interoperability.",
+    ///   "description": "This course demonstrates several best practices for Windows Forms development by taking a demo application and gradually improving it to improve the user experience and code quality. We'll see how you can improve the layout and resizing of your application, and how you can make it more accessible, usable, and navigable from the keyboard. We'll explore how to approach localization, exception handling, and threading. We'll also devote time to various patterns that will help you write more maintainable and testable code. Finally, we'll provide guidelines for creating your own custom controls, and see how you can interoperate other technologies such as hosting web and WPF content within a Windows Forms application.",
+    ///   "level": "Intermediate",
+    ///   "duration": "PT16562.2140000S",
+    ///   "rating": {
+    ///     ..
+    ///   },
+    ///   "popularityScore": 229685,
+    ///   "courseImageUrl": "https://pluralsight.imgix.net/course-images/windows-forms-best-practices-v1.jpg",
+    ///   "courseImage": {
+    ///     ..
+    ///   },
+    ///   "image": {
+    ///     ..
+    ///   },
+    ///   "audiences": [
+    ///     "software-development"
+    ///   ],
+    ///   "modules": [
+    ///     {
+    ///       ..
+    ///       "clips": [
+    ///         {
+    ///           ..
+    ///         },
+    ///         ..
+    ///       ]
+    ///     },
+    ///     ..
+    ///   ],
+    ///   "playerUrl": "/player?course=windows-forms-best-practices&author=mark-heath&name=windows-forms-best-practices-m1&clip=0&mode=live",
+    ///   "authors": [
+    ///     {
+    ///       ..
+    ///     }
+    ///   ],
+    ///   "skillPaths": [
+    ///     {
+    ///       ..
+    ///     }
+    ///   ],
+    ///   "hasLearningCheck": true,
+    ///   "hasTranscript": true
+    /// }
+    /// </example>
     [DataContract(Name = "course")]
     [Serializable]
     public sealed class Course
